6b6e64f2a7a6a0347bd59bd725f10fdf0d0ed43a
idward <idward@vip.sina.com>
2018-09-30 22:49:36 +0800

    update server.js

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.use((req, res, next) => {
     });
     next();
 });
-app.use((req, res, next) => {
-    res.render('maintenance');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance');
+// });
 //middleware for setting up public folder
 app.use(express.static(path.join(__dirname, 'public')));
 //route
