7865f4a901f22531323d585c6118a8dac2fc8399
idward <idward@vip.sina.com>
2018-10-05 18:35:53 +0800

    ignore config.json

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
 let env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TestTodoApp'
+if (env === 'development' || env === 'test') {
+    let config = require('./config.json');
+    let envc = config[env];
+    
+    Object.keys(envc).forEach(key => {
+        process.env[key] = envc[key];
+    });
 }
